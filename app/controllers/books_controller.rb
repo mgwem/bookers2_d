7f5741e92e03c6ff7a83ff2e44266a45b6aab87a
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-04-21 15:03:19 +0000

    [Update]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,19 @@
 class BooksController < ApplicationController
 
   def show
+    @booknew = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -40,6 +44,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
