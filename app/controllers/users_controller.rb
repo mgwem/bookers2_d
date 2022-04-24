bc06711cd6c438cccce95a17d8241c9ecd2dedd5
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-04-24 05:20:59 +0000

    [Update]ensure_correct_user変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:update, :edit]
 
   def show
     @user = User.find(params[:id])
@@ -15,11 +15,6 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    if @user == current_user
-      render :edit
-    else
-      redirect_to user_path(current_user)
-    end
   end
 
   def update
