2cbc3be11cdc47fa033748a036c0965d2cb24273
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-04-24 02:08:52 +0000

    aboutのルート表記変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root :to =>"homes#top"
   devise_for :users
-  get "home/about"=>"homes#about"
+  get "home/about"=>"homes#about", as: 'about'
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
